c18d383c0e28e87f6006e7379b7ac39c5ca3d215
akantoutsi <a.kantoutsi@gmail.com>
2019-07-13 00:26:04 +0300

    css for table

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,16 +3,44 @@ import React from 'react';
 import './Card.css';
 
 const Card = props => {
+    // const allCards = Array(52);
+
+    // for (let i=0; i<52; i++) {
+    //     allCards[i] = (
+    //         <li>
+    //             <div className="card back">*</div>
+    //         </li>
+    //     );
+    // }
+
     return (
         <div> 
             Card
             <div className="playingCards [fourColours|faceImages|simpleCards|inText|rotateHand]">
-            <strong>
-                <span className="card rank-a clubs">
-                    <span className="rank">A</span>
-                    <span className="suit">&clubs;</span>
-                </span>
-            </strong>
+                <ul className="deck">
+                    {/* {allCards} */}
+                    <li>
+                        <div class="card back">*</div>
+                    </li>
+                    <li>
+                        <div className="card back">*</div>
+                    </li>
+                    <li>
+                        <div className="card back">*</div>
+                    </li>
+                    <li>
+                        <div className="card back">*</div>
+                    </li>
+                
+                </ul>
+                <div className="clear"></div>
+
+                {/* <strong>
+                    <span className="card rank-a clubs">
+                        <span className="rank">A</span>
+                        <span className="suit">&clubs;</span>
+                    </span>
+                </strong> */}
             </div>
         </div>
     );
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import './Table.css';
+
 const Table = props => {
     return (
-        <div> 
-            Table
+        <div>
+            <div className='player-2'>
+                Player 2
+                <div></div>
+            </div>
+
+            <div className='player-3'>
+                Player 3
+                <div></div>
+            </div>
+
+            <div className='Table'></div>
+
+            <div className='player-1'>
+                Player 1
+                <div></div>
+            </div>    
+
+            <div className='player-4'>
+                Player 4
+                <div></div>
+            </div>
         </div>
     );
 }

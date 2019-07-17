8bec2f71faa0b0cc9d3a0369ff2656c591fc278b
akantoutsi <a.kantoutsi@gmail.com>
2019-07-17 23:08:21 +0300

    removed unecessary comments

--- src/containers/Table/Table.js
+++ src/containers/Table/Table.js
@@ -94,66 +94,10 @@ class Table extends Component {
 
         return (
             <div className='window-class'>
-                {/* <div className='player-info-2'>
-                    <div>Player 2</div>
-                    <hr />
-                    <div>
-                        $ 1.200
-                        <input type='number' min='0' step='0.1' />
-                        <button>Ok</button> 
-                    </div>
-                </div>
-
-                <div className='player-2'>
-                    <div className='center'>Seat Open / Player 2</div>
-                </div>
-
-                <div className='player-info-3'>
-                    <div>Player 3</div>
-                    <hr />
-                    <div>
-                        $ 1.200
-                        <input type='number' min='0' step='0.1' />
-                        <button>Ok</button> 
-                    </div>
-                </div>
-
-                <div className='player-3'>
-                    <div className='center'>Seat Open / Player 3</div>
-                </div> */}
-
                 <div className='Table'>
                     <Players />
                     <Board />
                 </div>
-
-                {/* <div className='player-info-1'>
-                    <div>Player 1</div>
-                    <hr />
-                    <div>
-                        $ 1.000
-                        <input type='number' min='0' step='0.1' />
-                        <button>Ok</button> 
-                    </div>
-                </div>
-
-                <div className='player-1'>
-                    <div className='center'>Seat Open / Player 1</div>
-                </div>    
-
-                <div className='player-info-4'>
-                    <div>Player 4</div>
-                    <hr />
-                    <div>
-                        $ 800
-                        <input type='number' min='0' step='0.1' />
-                        <button>Ok</button> 
-                    </div>
-                </div>
-
-                <div className='player-4'>
-                    <div className='center'>Seat Open / Player 4</div>
-                </div>*/}
             </div> 
         );
     }
